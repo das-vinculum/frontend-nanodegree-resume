d5932df444c97632d28aadcfea4f12b5732bd865
Florian Zeidler <zeidler.f@googlemail.com>
2017-04-05 06:01:16 +0200

    Add formatted name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var formattedName = HTMLheaderName.replace("%data%","Florian Zeidler")
+var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer")
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
